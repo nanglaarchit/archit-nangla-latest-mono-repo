2184d5100d1d7ed7459926301c80edd16daf8cb7
nanglaarchit <archit.nangla@sourcefuse.com>
2025-03-24 17:10:33 +0530

    Latest Commit

--- scripts/dependencies/install-aws-cli.sh
+++ scripts/dependencies/install-aws-cli.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Check and install AWS CLI if not present
 if ! command -v aws &> /dev/null; then
   cd /tmp
-  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+  # curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "/tmp/awscliv2.zip"
   unzip /tmp/awscliv2.zip
   /tmp/aws/install
 else
